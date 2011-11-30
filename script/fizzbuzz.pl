6fdf2666be07357e85b40c4bdc74e601d215608f
Ronnachate Gatekieo <yo@abctech-thailand.com>
2011-11-30 12:15:43 +0700

    Added script to call module.

---
+++ script/fizzbuzz.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use lib q{.};
+use strict;
+use warnings;
+use FizzBuzz;
+
+my $fb = FizzBuzz->new (
+    fizz => 3,
+    buzz => 5,
+);
+
+for(0..100) {
+    print $fb->get($_);
+    print "\n";
+}
+
+1;
